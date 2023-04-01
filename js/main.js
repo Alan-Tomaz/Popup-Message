bc10f78247ed9a4baee11f55b41b1543b8d5c5a8
Alan <alan4tomaz8@gmail.com>
2023-03-31 22:58:21 -0300

    Final Commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+function showConfirmMessage() {
+    popup.classList.add("open-popup");
+}
+
+function hideConfirmMessage() {
+    popup.classList.remove("open-popup");
+}
